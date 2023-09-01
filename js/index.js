b7c9faf80486228b665f118ffabfcc8368a9845f
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-09-01 20:11:57 +0600

    time added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const loadCategory = async () => {
     const fetchedCategory = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const response = await fetchedCategory.json()
-    console.log(response.data);
+    //console.log(response.data);
     displayCategory(response.data)
     loadCategoryContents(response.data[0].category_id)
 }
@@ -14,7 +14,7 @@ const displayCategory = (fetchedCategories) => {
     fetchedCategories.forEach(category => {
         const div = document.createElement('div')
         div.innerHTML = `
-        <button onclick='loadCategoryContents(${category.category_id})' class="bg-gray-300 px-4 py-2 font-medium rounded">${category.category}</button>`
+        <button onclick='loadCategoryContents(${category.category_id})' class="bg-gray-300 px-2 md:px-4 py-1 md:py-2 font-medium rounded">${category.category}</button>`
         categoryContainer.appendChild(div)
     });
 }
@@ -23,7 +23,7 @@ const displayCategory = (fetchedCategories) => {
 const loadCategoryContents = async (categoryId) => {
     const fetchCategoryContents = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const response = await fetchCategoryContents.json()
-    console.log(response.data);
+    //console.log(response.data);
     displayCategoryContents(response.data)
 
 }
@@ -32,17 +32,32 @@ const loadCategoryContents = async (categoryId) => {
 const displayCategoryContents = (contents) => {
     const cardContainer = document.getElementById('card-container')
     cardContainer.textContent = ""
-    
+    console.log(contents);
+    if(contents.length == 0){
+        const imgDiv = document.createElement('div')
+        imgDiv.innerHTML = `
+        <img class="cols-span-2" src="./images/Icon.png" alt="">
+        `
+        cardContainer.appendChild(imgDiv)
+    }
     contents.forEach(content => {
-        console.log(content.authors[0].verified);
-        //const imageSrc = ;
-        console.log(content);
+        //console.log(content.others.posted_date);
+        let hour = 0
+        let min = 0
+        if(content.others.posted_date){
+            const timeStringToNumber = parseInt(content.others.posted_date)
+            hour = timeStringToNumber / 60
+            min = timeStringToNumber % 60
+        }
+        let time = parseInt(hour)+"hrs "+parseInt(min)+"min ago"
+        
+        //console.log(time);
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="single-card-container ">
                 <div class="image-container relative mb-5">
                     <img class="rounded-lg h-40 w-full cover" src="${content.thumbnail}" alt="">
-                    <p id="time-duration" class="hidden absolute bottom-2 right-2 bg-black text-white rounded p-1">11hours 23mins</p>
+                    <p id="time-container" class="absolute bottom-2 right-2 bg-black text-white rounded">${content.others.posted_date?time:''}</p>
                 </div>
                 <div class="card-description-container flex gap-3">
                     <div class="profile-picture-container">
@@ -60,7 +75,10 @@ const displayCategoryContents = (contents) => {
                 </div>
             </div>
         `
+
         cardContainer.appendChild(div)
+        const setInvisible = document.getElementById('time-container')
+        
     })
 
 }
